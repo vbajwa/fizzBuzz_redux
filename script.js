8b6ed6b4d15c4dee792d656d664c02aa8bae5a60
Vijender Singh Bajwa <vbajwamd@gmail.com>
2016-02-17 14:03:16 -0500

    initial and final commit

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-var fizzBuzzRedux = function () {
-  // what arguments should add in above?
-  // How do you use the arguments to create fizzBuzz?;
+var fizzBuzzRedux = function (num, word1, word2) {
+  for (var i =1; i <= num; i = i + 1){
+    if (i % 15 === 0){
+      console.log(word1 + word2);
+    }
+    else if (i % 3 ===0){
+        console.log(word1);
+    }
+    else if (i % 5 ===0){
+        console.log(word2);
+    }
+    else {
+      console.log(i)
+    }
+  }
 }
